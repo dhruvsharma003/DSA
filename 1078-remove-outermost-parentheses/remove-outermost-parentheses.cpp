80e3449a8703c0ff64ed68e3a10a2f39dc1616aa
dhruvsharma003 <160222187+dhruvsharma003@users.noreply.github.com>
2026-04-09 21:21:17 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (54.24%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string res="";
+        int level=0;
+        for(char ch:s){
+            if(ch=='('){
+                if(level>0){
+                    res+=ch;
+                }
+                level++;
+            }
+            if(ch==')'){
+                level--;
+                if(level>0){
+                    res+=ch;
+                }
+            }
+        }
+        return res;
+    }
+};
